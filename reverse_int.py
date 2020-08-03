adccf0683908d50e6f4c3a7c1151ee6f685791a2
KEAN Mentor <kesztea@gmail.com>
2020-08-03 16:21:07 +0200

    Adding the solution for the first problem.

---
+++ reverse_int.py
@@ -0,0 +1,22 @@
+"""Given an integer, return the integer with reversed digits.
+Note: The integer could be either positive or negative."""
+
+
+import sys
+
+
+def solution(x):
+    string_version = str(x)
+
+    if string_version[0] == "-":
+        return "-" + string_version[:0:-1]
+    else:
+        return string_version[::-1]
+
+
+if __name__ == '__main__':
+    try:
+        user_input = int(sys.argv[1])
+        print(solution(user_input))
+    except ValueError:
+        print("Your input is not an integer.")
